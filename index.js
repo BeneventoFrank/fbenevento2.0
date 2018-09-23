f96fa8628f06f34c5cb9aba0d38cb3e3bc3edb81
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-23 11:09:07 -0700

    fixed mobile bug

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+
+let btn = document.querySelector('#showAddJobExp')
+btn.addEventListener('click',(element)=>{
+     let list = document.querySelectorAll('.fullViewOnly');
+     list.forEach(e=>{e.classList.toggle('fullViewOnly')});
+     btn.classList.add('hidden');
+})
